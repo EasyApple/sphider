b4512bab9370aa2d4673bfd2cde5d7cb34a8398b
music_space@yahoo.cn <music_space@yahoo.cn@d31f7b45-1e65-4d9b-a763-6d279f81bfe5>
2012-11-08 02:33:05 +0000

    Update DB Setting

--- 1/settings/database.php
+++ 1/settings/database.php
@@ -28,12 +28,12 @@
 	*/
 
     /* 连主库 */
-	$success = mysql_pconnect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
+	$link = mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
     /* 连从库 */
-	//$success = mysql_connect(SAE_MYSQL_HOST_S.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
-	if (!$success)
+	//$link = mysql_connect(SAE_MYSQL_HOST_S.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
+	if (!$link)
 		die ("<b>Cannot connect to database, check if username, password and host are correct.</b>");
-    $success = mysql_select_db (SAE_MYSQL_DB);
+    $success = mysql_select_db (SAE_MYSQL_DB,$link);
 	if (!$success) {
 		print "<b>Cannot choose database, check if database name is correct.";
 		die();
