25e286b33249c6d2d26292e66cd1319ccc066f42
Jack <jack@easyapple.net>
2012-11-08 04:13:56 +0000

    Update Info

--- 1/search.php
+++ 1/search.php
@@ -105,7 +105,7 @@ function poweredby () {
     //If you want to remove this, please donate to the project at http://www.sphider.eu/donate.php
     print $sph_messages['Powered by'];?>
 	<a href="http://www.sphider.eu/"><img src="sphider-logo.png" border="0" style="vertical-align: middle" alt="Sphider"></a>
-    <Label>Transplant By</Label>
+    <Label>Transplant by</Label>
 	<a href="http://www.easyapple.net/"><img src="easyapple-logo.png" border="0" style="vertical-align: middle" alt="Sphider"></a>
 	<?php 
 }
