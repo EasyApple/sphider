d1e307c49d44ad27b131690b3cebba7c6a372f6d
Jack <jack@easyapple.net>
2012-11-08 04:17:57 +0000

    Update Info

--- 1/index.php
+++ 1/index.php
@@ -1,5 +1,5 @@
 <?php
-print "<b>Welcome to Sphider SAE! </b>";
-print "<b>Go to <a href=\"search.php\">search.php</a> to start sphider.</b>";
-print "<b>Or go to <a href=\"admin\admin.php\">admin.php</a> to start configer.</b>";
+print "<b>Welcome to Sphider SAE! </b><br/>";
+print "<b>Go to <a href=\"search.php\">search.php</a> to start sphider.</b><br/>";
+print "<b>Or go to <a href=\"admin\admin.php\">admin.php</a> to start configer.</b><br/>";
 
