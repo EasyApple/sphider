8313c805c15064985807b5370edcf5d9d108633e
music_space@yahoo.cn <music_space@yahoo.cn@d31f7b45-1e65-4d9b-a763-6d279f81bfe5>
2012-11-08 04:13:56 +0000

    Update Info

--- 1/index.php
+++ 1/index.php
@@ -1,2 +1,5 @@
 <?php
-print "<b>Welcome to Sphider SAE! Go to <a href=\"search.php\">search.php</a> to start sphider.</b>";
+print "<b>Welcome to Sphider SAE! </b>";
+print "<b>Go to <a href=\"search.php\">search.php</a> to start sphider.</b>";
+print "<b>Or go to <a href=\"admin\admin.php\">admin.php</a> to start configer.</b>";
+
