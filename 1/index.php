490b1c4753545c68e3143975a66c40baccf06166
Jack <jack@easyapple.net>
2012-11-08 04:21:44 +0000

    Update Info

--- 1/index.php
+++ 1/index.php
@@ -1,8 +1,9 @@
-<center>
+<center><font size=32 color="blue">
 <?php
-print "<b>Welcome to Sphider for SAE! </b><br/>";
-print "<b>Go to <a href=\"search.php\">search.php</a> to start sphider.</b><br/>";
-print "<b>Or go to <a href=\"admin\admin.php\">admin.php</a> to start configer.</b><br/>";
+print "<br/<br/<br/<br/<br/><br/>";
+print "<br/><b>Welcome to Sphider for SAE! </b><br/>";
+print "<br/><b>Go to <a href=\"search.php\">search.php</a> to start sphider.</b><br/>";
+print "<br/><b>Or go to <a href=\"admin\admin.php\">admin.php</a> to start configer.</b><br/>";
 ?>
-</center>
+</font></center>
 
