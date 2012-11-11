dcc99c1440ce4cd8692da3c77bf561cc36198558
Jack <jack@easyapple.net>
2012-11-11 10:12:05 +0000

    Update Settings

--- 1/settings/database.php
+++ 1/settings/database.php
@@ -16,20 +16,9 @@
 	}
 	*/
 
-	/*
-	如果您没有使用SaeMysql类操作数据库，可以使用以下预定义常量进行连接：
-	用户名　 :  SAE_MYSQL_USER
-	密　　码 :  SAE_MYSQL_PASS
-	主库域名 :  SAE_MYSQL_HOST_M
-	从库域名 :  SAE_MYSQL_HOST_S
-	端　　口 :  SAE_MYSQL_PORT
-	数据库名 :  SAE_MYSQL_DB
-    使用方法，以mysql模块为例:
-	*/
-
-    /* 连主库 */
+    /*Connect Primary DB*/
 	$link = mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
-    /* 连从库 */
+    /*Connect Sub DB*/
 	//$link = mysql_connect(SAE_MYSQL_HOST_S.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
 	if (!$link)
 		die ("<b>Cannot connect to database, check if username, password and host are correct.</b>");
